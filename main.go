a685bf1ed12c4a0ca42cb36c4c10129d6f700936
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2024-01-13 19:57:39 -0300

    Load config from file & environment variables in Go with Viper

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 
 	"github.com/felipeazsantos/simple_bank/api"
 	db "github.com/felipeazsantos/simple_bank/db/sqlc"
+	"github.com/felipeazsantos/simple_bank/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5442/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8090"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
 	}
@@ -25,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
